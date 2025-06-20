3eabf795fedd2d8902a72330033cc4a9129201cb
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2025-06-20 18:04:17 +0530

    voting dapp

--- proj-2-voting-dapp-blinks/programs/proj-2-voting-dapp-blinks/src/lib.rs
+++ proj-2-voting-dapp-blinks/programs/proj-2-voting-dapp-blinks/src/lib.rs
@@ -6,10 +6,44 @@ declare_id!("8aMGnq2gvuahCELse4FEjZgJiQMtiF75oAGbYGt3DGPh");
 pub mod proj_2_voting_dapp_blinks {
     use super::*;
     
-    pub fn initialize_poll(xtx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
+    pub fn initialize_poll(ctx: Context<InitializePoll>, 
+                                poll_id: u64,
+                                description: String,
+                                title: String,
+                                poll_start: u64,
+                                poll_end: u64) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+//         ctx.accounts: This accesses the accounts struct that you defined in your InitializePoll struct
+//  ctx.accounts.poll: This specifically accesses the poll account from your accounts struct
+
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.title = title;
+        poll.poll_start = poll_start;
+        poll.poll_end = poll_end;
+        poll.candidate_amount = 0;
+ 
+        Ok(())
+    }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, candidate_name: String, poll_id: u64) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        let poll = &mut ctx.accounts.poll;
+        poll.candidate_amount += 1;
+        candidate.candidate_name = candidate_name;
+        candidate.candidate_votes = 0;
+        
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, poll_id: u64, candidate_name: String) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        candidate.candidate_votes += 1;
+
         Ok(())
     }
 }
+
 
 
 #[derive(Accounts)]
@@ -17,10 +51,10 @@ pub mod proj_2_voting_dapp_blinks {
 pub struct InitializePoll<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
-    #[account(init, payer = signer, space = 8 + poll::INIT_SPACE, seeds = [poll_id.to_le_bytes().as_ref()], bump)]
+    #[account(init, payer = signer, space = 8 + Poll::INIT_SPACE, seeds = [poll_id.to_le_bytes().as_ref()], bump)]
     pub poll: Account<'info, Poll>,
+    pub system_program: Program<'info, System>,
 }
-
 
 #[account]
 #[derive(InitSpace)]
@@ -32,4 +66,32 @@ pub struct Poll{
     pub title: String,
     pub poll_start: u64,
     pub poll_end: u64,
+    pub candidate_amount: u64,
 } 
+
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut, seeds = [poll_id.to_le_bytes().as_ref()], bump)]
+    pub poll: Account<'info, Poll>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Candidate::INIT_SPACE,
+        seeds = [b"candidate", poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
+        bump
+    )]
+    pub candidate: Account<'info, Candidate>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate{
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
